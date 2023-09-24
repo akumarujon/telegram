c1bd3d48995ff6e1a3964e2349ebcf061f53b30d
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-09-24 19:16:56 +0500

    no need to warn for topicness

--- src/hooks/is-reply.ts
+++ src/hooks/is-reply.ts
@@ -3,12 +3,12 @@ import { Context, NextFunction } from "grammy";
 import topics from "../../data/topics.json" assert { type: "json" };
 
 export default async (ctx: Context, next: NextFunction) => {
-  if (!ctx.message?.is_topic_message) {
-    return await reply(
-      ctx,
-      `⚠️ <b>We are not in topic to use this command!</b>`,
-    );
-  }
+  // if (!ctx.message?.is_topic_message) {
+  //   return await reply(
+  //     ctx,
+  //     `⚠️ <b>We are not in topic to use this command!</b>`,
+  //   );
+  // }
 
   if (
     !ctx.message?.reply_to_message ||
