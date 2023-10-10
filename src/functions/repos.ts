6c51bc909c079e8e716518951daf7693ee829cde
akumaru <isakulovdev@gmail.com>
2023-10-10 22:10:59 +0500

    fixed problem with inline query result

--- src/functions/repos.ts
+++ src/functions/repos.ts
@@ -1,4 +1,5 @@
-import { Composer, Context, InlineQueryResultBuilder } from "grammy";
+import { Composer, Context } from "grammy";
+import { InlineQueryResultArticle } from "grammy/types";
 import { v4 } from "uuid";
 import { find } from "@/utils/repos";
 import { Repo } from "@/types/repo";
@@ -9,20 +10,27 @@ composer.on("inline_query", async (ctx: Context) => {
   const query = ctx.inlineQuery!.query;
   const repos = await find(query);
 
-  const result = repos.map((repo: Repo) =>
-    InlineQueryResultBuilder.article(v4() as string, repo.name, {
-      description: `Description: ${repo.description}`,
-    }).text(
-      `Repository: ${repo.name}\n` +
-        `URL: ${repo.html_url}\n` +
-        `Description: ${repo.description}\n` +
-        `Stars count: ${repo.stargazers_count}\n` +
-        `Language: ${repo.language || "None."}\n` +
-        `Forks: ${repo.forks}\n` +
-        `Open issues: ${repo.open_issues}\n` +
-        `Topics: ${repo.topics?.join(",") || "None."}`
-    )
-  );
+  let result: InlineQueryResultArticle[] = [];
+
+  repos.map((repo: Repo) => {
+    result.push({
+      type: "article",
+      id: v4() as string,
+      title: repo.name,
+      input_message_content: {
+        message_text:
+          `Repository: ${repo.name}\n` +
+          `URL: ${repo.html_url}\n` +
+          `Description: ${repo.description}\n` +
+          `Stars count: ${repo.stargazers_count}\n` +
+          `Language: ${repo.language || "None."}\n` +
+          `Forks: ${repo.forks}\n` +
+          `Open issues: ${repo.open_issues}\n` +
+          `Topics: ${repo.topics?.join(",") || "None."}`,
+      },
+      description: repo.description,
+    });
+  });
 
   await ctx.answerInlineQuery(result);
 });
